080f49baea055cdb9dccba44867a4b7dc6920345
Logan Wood <lgnwood@gmail.com>
2023-10-20 23:38:39 -0600

    complete tests

--- index.js
+++ index.js
@@ -12,5 +12,27 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+ let newArray = [...tutorials]
+ let temp = ''
+ let temp2 = ''
+
+ for (let j = 0; j < newArray.length; j++) {
+    let test = newArray[j].split(' ')
+    let test2 = test.map((el) => el.charAt(0).toUpperCase() + el.slice(1))
+    temp = []
+    for (let i = 0; i < test2.length; i++) {
+
+      if (i !== test2.length - 1) {
+        temp += test2[i] + ' '
+        }else {
+        temp += test2[i]
+        }
+      temp2 = temp 
+    }
+
+  newArray[j] = temp2
+ }
+return newArray
 }
+
+
